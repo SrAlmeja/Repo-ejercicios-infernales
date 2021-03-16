31dadb326607db028c01b863dfc7b64b8bd1ad18
Angel Legorreta <legosurio@hotmail.com>
2021-03-15 20:09:00 -0600

    working on the triangle

--- Assets/Code/1 Figures/Cirlce.cs
+++ Assets/Code/1 Figures/Cirlce.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Cirlce : MonoBehaviour
+public class Circle
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int pi;
+    private float x1, x2;
+    private float radious, diameter, perimeter, area;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetRadious();
+        GetDiameter();
+        GetPerimeter();
+        GetArea();
+    }
+
+    public void GetRadious()
+    {
+        radious = x1 - x2;
+        Debug.Log("El radio de tu circulo mide [0]" + radious);
+    }
+
+    public void GetDiameter()
+    {
+        diameter = radious *2;
+        Debug.Log("El diametro de tu circulo mide [0]" + diameter);
+    }
+
+    public void GetPerimeter()
+    {
+        perimeter = 3.1416f * diameter;
+        Debug.Log("El perimetro de tu rectangulo es [0]" + perimeter);
+    }
+
+    public void GetArea()
+    {
+        area = 3.1416f * (radious * radious);
+        Debug.Log("El area de tu circulo es [0]" + area);
     }
 }
--- Assets/Code/1 Figures/Triangle.cs
+++ Assets/Code/1 Figures/Triangle.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Triangle : MonoBehaviour
+public class Triangle
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int side1, side2, side3;
+    private int x1, x2, y1;
+    private float height, theBase, perimeter, area;
+    private string triangleType;
+    private bool equilateral, isosceles, scalene;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetTriangle();
+        GetHeight();
+        GetPerimeter();
+        GetArea();
+    }
+
+    public void Side1()
+    {
+        side1 = x1 - x2;
+    }
+
+    public void Side2()
+    {
+        side2 = x1 - y1;
+    }
+
+    public void Side3()
+    {
+        side3 = x2 - y1;
+    }
+
+    public void GetTriangle()
+    {
+
+    }
+
+    public void GetHeight()
+    {
+        height = y1 - y2;
+        Debug.Log("El lado 2 y 4 de tu rectangulo mide [0]" + height);
+    }
+
+    public void GetPerimeter()
+    {
+        perimeter = (theBase * 2) + (height * 2);
+        Debug.Log("El perimetro de tu rectangulo es [0]" + perimeter);
+    }
+
+    public void GetArea()
+    {
+        area = theBase * height;
+        Debug.Log("El area de tu rectangulo es [0]" + area);
     }
 }
