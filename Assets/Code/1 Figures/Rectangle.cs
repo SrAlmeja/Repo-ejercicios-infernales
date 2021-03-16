c4a27e5fbe452362903db27c12c68f95ee92f81e
Angel Legorreta <legosurio@hotmail.com>
2021-03-15 18:22:17 -0600

    Working on rectangle

---
+++ Assets/Code/1 Figures/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Rectangle
+{
+    //Atributes
+    private int widthSideLenght, longSideLeight, baseResult, perimeter, area;
+    private int x1, x2, x3, x4, y1, y2, y3, y4;
+
+
+
+    public int WidthSide
+    {
+        get { return widthSideLenght;  }
+        set
+        {
+            if (value == 0)
+            {
+                widthSideLenght = value;
+            }
+            else
+            {
+                widthSideLenght = 0;
+            }
+        }
+    }
+
+    public int LongSide
+    {
+        get { return widthSideLenght; }
+        set
+        {
+            if (value == 0)
+            {
+                widthSideLenght = value;
+            }
+            else
+            {
+                widthSideLenght = 0;
+            }
+        }
+    }
+}
