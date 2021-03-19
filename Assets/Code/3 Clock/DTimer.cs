9568b861fa636ffea542cc0ddce5e5cc9dfe7d3b
Angel Legorreta <legosurio@hotmail.com>
2021-03-18 22:08:53 -0600

    working on clock

---
+++ Assets/Code/3 Clock/DTimer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DTimer
+{
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public DTimer(int ahours, int aminutes, int aseconds)
+    {
+        hours = ahours;
+        minutes = aminutes;
+        seconds = aseconds;
+    }
+    
+    //getTime & getTime
+    public int Hours
+    {
+        get { return hours; }
+        set { hours = value; }
+
+    }
+
+    public int Minutes
+    {
+        get { return minutes; }
+        set { minutes = value; }
+    }
+
+    public int Seconds
+    {
+        get { return seconds; }
+        set
+        { seconds = value; }
+    }
+
+
+
+
+// Start is called before the first frame update
+void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetTime(float secondsTime)
+    {
+        if (secondsTime < 0) secondsTime = 0;
+
+        hours = (int)secondsTime / 3600;
+        minutes = (int)secondsTime / 60;
+        seconds = (int)secondsTime % 60;
+    }
+}
+
---
+++ Assets/Code/3 Clock/TheTime.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TheTime : MonoBehaviour
+{
+    private int inicialTime;
+    private int hour, minute, second;
+    public Text clock;
+    private float timeSpeed = 1;
+    private float hoursTime = 0f;
+    private float minutesTime = 0f;
+    private float secondsTime = 0f;
+    private float scaleInicialTime;
+    private float realFrameTime = 0f;
+
+    public TheTime(int ahour, int aminute, int asecond, int atimeUnit)
+    {
+        hour = ahour;
+        minute = aminute;
+        second = asecond;
+    }
+
+    void Star()
+    {
+        timeSpeed = scaleInicialTime;
+
+        clock = GetComponent<Text>();
+
+        secondsTime = inicialTime;
+
+        GetTime(showmeTime);
+
+        
+    }
+
+    private void Update()
+    {
+        realFrameTime = Time.deltaTime * timeSpeed;
+
+        secondsTime += realFrameTime;
+
+        GetTime(showmeTime);
+
+    }
+
+    public void GetTime(float showmeTime)
+    {
+        if (showmeTime < 0) showmeTime = 0;
+
+        hour = (int)minutesTime / 3600;
+        minute = (int)secondsTime / 60;
+        second = (int)secondsTime % 60;
+
+        clock.text = hour + ":" + minute + ":" + second;
+    }
+
+}
