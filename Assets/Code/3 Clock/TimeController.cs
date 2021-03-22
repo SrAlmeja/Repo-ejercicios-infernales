f1c4ccb53eddd4839ae5f3f7d4cb2eacc569845c
Angel Legorreta <legosurio@hotmail.com>
2021-03-22 01:20:33 -0600

    now, i'm dead x(

--- Assets/Code/3 Clock/TimeController.cs
+++ Assets/Code/3 Clock/TimeController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour
 {
-    TheTime timeController = new TheTime(0, 0, 0);
+    TheTime timeController = new TheTime(00, 00, 00);
 
     private const float secondsOnADay = 86400f;
     private float day;
@@ -14,11 +14,11 @@ public class TimeController : MonoBehaviour
     public int changeTime;
     public float TimeScale = 1;
 
-    public Text textTime;
+    public Text InfernalClock;
 
     private void Awake()
     {
-        if (textTime == null) textTime = GetComponent<Text>();
+        if (InfernalClock == null) InfernalClock = GetComponent<Text>();
     }
     void FixedUpdate()
     {
@@ -28,17 +28,17 @@ public class TimeController : MonoBehaviour
     {
         day += Time.deltaTime / secondsOnADay;
 
-        float normalizedDay = day % 1f;
-        float hoursInADay = 24f;
+        float normalizedDay = day % 1;
+        float hoursInADay = 24;
 
         timeController.GetHour = Mathf.Floor(normalizedDay * hoursInADay);
 
-        float minutesInHour = 60f;
-        timeController.GetMinute = Mathf.Floor(((normalizedDay * hoursInADay) % 1f) * minutesInHour);
-        float secondsInMinute = 60f;
-        timeController.GetSecond = Mathf.Floor(((((normalizedDay * hoursInADay) % 1f) * minutesInHour) % 1f) * secondsInMinute);
+        float minutesInHour = 60;
+        timeController.GetMinute = Mathf.Floor(((normalizedDay * hoursInADay) % 1) * minutesInHour);
+        float secondsInMinute = 60;
+        timeController.GetSecond = Mathf.Floor(((((normalizedDay * hoursInADay) % 1) * minutesInHour) % 1) * secondsInMinute);
 
-        textTime.text = timeController.GetHour + ":" + timeController.GetMinute + " : " + timeController.GetSecond;
+        InfernalClock.text = timeController.GetHour + ":" + timeController.GetMinute + " : " + timeController.GetSecond;
     }
 
     public void ChangeTime()
@@ -47,6 +47,6 @@ public class TimeController : MonoBehaviour
         timeController.GetMinute = theMinute;
         timeController.GetHour = theHour;
         Debug.Log(timeController.GetHour + ":" + timeController.GetMinute + " : " + timeController.GetSecond);
-        textTime.text = timeController.GetHour + ":" + timeController.GetMinute + " : " + timeController.GetSecond;
+        InfernalClock.text = timeController.GetHour + ":" + timeController.GetMinute + " : " + timeController.GetSecond;
     }
 }
